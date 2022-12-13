5d4412579bde4d828b41664120043f86e5e85b1b
Aleksandr <bestprogreraxe@gmail.com>
2022-12-13 11:04:20 +0300

    solved Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,6 +1,53 @@
-﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿/* Задача 29: 
 
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-
 6, 1, 33 -> [6, 1, 33] */
 
+double GetNumber(string message)
+{
+    bool isCorrect = false;
+    double result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (double.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Error: wrong entered data.");
+        }
+    }
+    return result;
+}
+
+double[] FillingArray()
+{
+    double[] array = new double[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        double number = GetNumber("Enter number for input to array:");
+        array[i] = number;
+    }
+
+
+    Console.Write("Your array: [");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}].");
+        }
+    }
+    return array;
+}
+double[] outArray = new double[8];
+outArray = FillingArray();
